345f8c164db2f3a78392fb60e64976c94cf2ddca
Zack Tang <zacktang@tencent.com>
2026-01-16 17:51:51 +0800

    chore: 更新云开发配置
    - 更新 AppID: wxf5c638586169a8c5
    - 更新云环境ID: cloud1-9g0875ja139e619f

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -24,7 +24,7 @@ App({
         console.error('请使用 2.2.3 或以上的基础库以使用云能力')
       } else {
         wx.cloud.init({
-          env: 'your-env-id', // 替换为实际的云开发环境ID
+          env: 'cloud1-9g0875ja139e619f', // 云开发环境ID
           traceUser: true
         })
       }
